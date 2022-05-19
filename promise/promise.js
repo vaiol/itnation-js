89f277b33df8dab4cdd9e23ccdaf36d39fae9343
Alexander Stepanov <alexandr.stepanov@koyfin.com>
2022-05-19 21:28:35 +0300

    fix

--- promise/promise.js
+++ promise/promise.js
@@ -1,20 +1,20 @@
 const fs = require('fs')
 
-// fs.writeFile('./test.txt', 'Hello ITNATION', (err, data) => {
-//     if (err) {
-//         console.error(err)
-//         return
-//     }
-//     console.log('writeEnd', err, data)
+fs.writeFile('./test.txt', 'Hello ITNATION', (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    console.log('writeEnd', err, data)
 
-//     console.log('readStart')
-//     fs.readFile('./test.txt', 'utf-8', (err, data) => {
-//         console.log('readEnd', data)
-//         fs.writeFile('./test2.txt', data + '1', (err, data) => {
-//             console.log('write2end')
-//         })
-//     })
-// })
+    console.log('readStart')
+    fs.readFile('./test.txt', 'utf-8', (err, data) => {
+        console.log('readEnd', data)
+        fs.writeFile('./test2.txt', data + '1', (err, data) => {
+            console.log('write2end')
+        })
+    })
+})
 
 const myWriteFile = (path, data) => {
     return new Promise((resolve, reject) => {
